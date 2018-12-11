161fd4e0efe6ff91c17b3d75094e689f915c616e
VictorGorban <VictorGorban2@ya.ru>
2018-12-12 00:45:24 +0300

    Tnnx to Yury.

---
+++ Extras/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extras
+{
+	public static class ExtraConverters
+	{
+		public static string ToUnicodeString(this byte[] bytes)
+		{
+			var str = Encoding.Unicode.GetString(bytes).Replace("\0", "");
+
+			return str;
+		}
+		public static byte[] ToUnicodeBytes(this string str, int requiredCountOfBytes)
+		{
+			return Encoding.Unicode.GetBytes(str).TrimOrExpandTo(requiredCountOfBytes);
+		}
+		public static T[] TrimOrExpandTo<T>(this T[] array, int requiredLength)
+		{
+			var list = array.ToList();
+			if (list.Count < requiredLength)
+			{
+				list.AddRange(new T[requiredLength - list.Count]);
+			}
+			else if (list.Count > requiredLength)
+			{
+				list.RemoveRange(requiredLength, list.Count - requiredLength);
+			}
+			return list.ToArray();
+		}
+	}
+
+	public class FileHeader
+	{
+		public Byte WriteAllowed;
+		public Byte ReadAllowed;
+		public static int SizeInBytes = 2;
+	}
+}
---
+++ IPC Read/Program.cs
@@ -0,0 +1,39 @@
+﻿using Extras;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using static System.Threading.Thread;
+
+
+
+namespace IPC_Read
+{
+	class Program
+	{
+		public const int reqNumBytes = 400;
+
+		static void Main(string[] args)
+		{
+			Console.Title = "Read";
+
+			while (true)
+			{
+				WriteLine("Нажмите Enter чтобы считать данные из общей памяти");
+				ReadLine();
+
+				var result = ReadFromSharedMemoryWithSleep();
+				WriteLine(result);
+			}
+		}
+
+		public static string ReadFromSharedMemoryWithSleep()
+		{
+			
+			return "OK";
+		}
+	}
+
+}
