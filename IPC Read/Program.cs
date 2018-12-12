c21fc61b46a902a829ce74845ef52995a9af4ec8
VictorGorban <VictorGorban2@ya.ru>
2018-12-12 13:48:40 +0300

    All works

--- IPC Read/Program.cs
+++ IPC Read/Program.cs
@@ -1,8 +1,10 @@
 ﻿using Extras;
 using System;
 using System.Collections.Generic;
+using System.IO.MemoryMappedFiles;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using static System.Console;
 using static System.Threading.Thread;
@@ -15,24 +17,52 @@ namespace IPC_Read
 	{
 		public const int reqNumBytes = 400;
 
+		// Если в системе уже существует семафор с данным именем, то не пересоздает.
+		public static Semaphore pool = new Semaphore(1, 1, "MySystemwideSemaphore");
+
 		static void Main(string[] args)
 		{
 			Console.Title = "Read";
 
-			while (true)
-			{
-				WriteLine("Нажмите Enter чтобы считать данные из общей памяти");
-				ReadLine();
 
-				var result = ReadFromSharedMemoryWithSleep();
-				WriteLine(result);
+			using (MemoryMappedFile.CreateOrOpen("MyMmf", 400 + FileHeader.SizeInBytes))
+			{
+				while (true)
+				{
+					WriteLine("Нажмите Enter чтобы считать данные из общей памяти");
+					ReadLine();
+
+					var result = ReadFromSharedMemoryWithSleep(MemoryMappedFile.CreateOrOpen("MyMmf", 400 + FileHeader.SizeInBytes));
+					WriteLine(result);
+				} 
 			}
 		}
 
-		public static string ReadFromSharedMemoryWithSleep()
+		public static string ReadFromSharedMemoryWithSleep(MemoryMappedFile mmf)
 		{
-			
-			return "OK";
+			// создаю view для header
+			// считываю header, проверяю, что читать можно
+			// если нет, return "Не могу прочитать, занято";
+			// изменяю header, что нельзя писать
+			// записываю изменный header
+			// записываю данные
+			// опять изменяю header на писать можно
+			// записываю изменный header
+
+			// Так, а теперь семафор
+			pool.WaitOne();
+
+			var view = mmf.CreateViewAccessor();
+			var bytes = new byte[reqNumBytes];
+			view.ReadArray(0, bytes, 0, bytes.Length);
+
+			WriteLine("Sleeping 2 sec");
+			Sleep(2000);
+			pool.Release();
+
+			var str = bytes.ToUnicodeString();
+			return str;
+
 		}
 	}
 
--- IPC Write/Program.cs
+++ IPC Write/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Extras;
 using System;
+using System.IO.MemoryMappedFiles;
+using System.Threading;
 using static System.Console;
 using static System.Threading.Thread;
 
@@ -9,23 +11,43 @@ namespace IPC_Read
 	{
 		public const int reqNumBytes = 400;
 
+		// Если в системе уже существует семафор с данным именем, то не пересоздает.
+		public static Semaphore pool = new Semaphore(1, 1, "MySystemwideSemaphore");
+
 		static void Main(string[] args)
 		{
 			Console.Title = "Write";
 
-			while (true)
+			using (var mmf = MemoryMappedFile.CreateOrOpen("MyMmf", reqNumBytes))
 			{
-				WriteLine("Введите строку и нажмите Enter чтобы записать эту строку в общую память");
-				var strToWrite = ReadLine();
+				while (true)
+				{
+					WriteLine("Введите строку и нажмите Enter чтобы записать эту строку в общую память");
+					var strToWrite = ReadLine();
 
-				var result = WriteToSharedMemoryWithSleep(strToWrite);
-				WriteLine(result);
+					var result = WriteToSharedMemoryWithSleep(mmf, strToWrite);
+					WriteLine(result);
+				} 
 			}
+			// Когда последний поток/процесс завершит работу, файл уничтожится сборщиком мусора.
 		}
 
-		public static string WriteToSharedMemoryWithSleep(string str)
+		public static string WriteToSharedMemoryWithSleep(MemoryMappedFile mmf, string str)
 		{
 			
+
+			// Так, а теперь семафор
+			pool.WaitOne();
+
+			var view = mmf.CreateViewAccessor();
+			var bytes = str.ToUnicodeBytes(reqNumBytes);
+			view.WriteArray(0, bytes, 0, bytes.Length);
+
+			WriteLine("Sleeping 4 sec");
+			Sleep(4000);
+			pool.Release();
+
+
 			return "OK";
 		}
 	}
